318cad5c36cfa3a55d95ac82a5ddf7b398a49613
ajgarcia09 <anitajgr@gmail.com>
2018-03-24 23:30:56 -0600

    Successfully added the list adapter with fake data

--- app/src/main/java/edu/utep/cs/cs4330/dandelion/MainFragment.java
+++ app/src/main/java/edu/utep/cs/cs4330/dandelion/MainFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ajgarcia09 on 3/24/18.
@@ -15,6 +19,24 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         //inflate the layer for this fragment
-        return inflater.inflate(R.layout.fragment_main, container,false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container,false);
+
+        ArrayList<String> weekForecast = new ArrayList<String>();
+        weekForecast.add("Today - Sunny - 88/63");
+        weekForecast.add("Tomorrow - Foggy - 70/46");
+        weekForecast.add("Monday - Cloudy - 60/75");
+        weekForecast.add("Tuesday - Rainy - 65/80");
+        weekForecast.add("Wednesday - Sunny - 70/85");
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(this.getActivity(),R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,weekForecast);
+
+        //Get a reference to the listView, attach it to this adapter.
+        //Will supply list item layouts to  the list view based on weekForecast
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+        return rootView;
+
+
     }
 }
