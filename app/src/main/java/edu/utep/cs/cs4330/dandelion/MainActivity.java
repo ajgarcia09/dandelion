cfa6ff971f404781b0587e19c78fff8b9a671bc2
ajgarcia09 <anitajgr@gmail.com>
2018-04-23 00:32:36 -0600

    Successfully added settings activity

--- app/src/main/java/edu/utep/cs/cs4330/dandelion/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/dandelion/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.utep.cs.cs4330.dandelion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +13,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }*/
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /**Handle action bar item clicks here. The action bar will
+         * automatically handle clicks on the Home/Up button, so as
+         * long as you specify a parent activity in AndroidManifest.xml.
+         */
+        int id = item.getItemId(); // which item was selected?
+        if(id == R.id.action_settings){
+            startActivity(new Intent(this,SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
